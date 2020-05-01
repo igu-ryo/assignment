b4c4d6c60a27ae4fe4fcacf01e8eba4e6648d712
igu-ryo <49966687+igu-ryo@users.noreply.github.com>
2020-05-01 15:08:31 +0900

    フラクタル図形を関数で描く

--- CG_01/ソース.cpp
+++ CG_01/ソース.cpp
@@ -1,11 +1,52 @@
 #define FREEGLUT_STATIC
 #define _USE_MATH_DEFINES
+#include <iostream>
 #include <GL/glut.h>
-#include <math.h>
+#include <cmath>
+using namespace std;
 
-void display(void);
-void donut(double);
+void kochCurve(double x1, double y1, double x2, double y2)
+{
+	double xx1, xx2, yy1, yy2, xx3, yy3, len, rad60;
 
+	// 3等分する点の座標
+	xx1 = x1 + (x2 - x1) * (1.0 / 3.0);
+	yy1 = y1 + (y2 - y1) * (1.0 / 3.0);
+	xx2 = x1 + (x2 - x1) * (2.0 / 3.0);
+	yy2 = y1 + (y2 - y1) * (2.0 / 3.0);
+
+	len = sqrt(pow(xx1 - xx2, 2.0) + pow(yy1 - yy2, 2.0));// 3等分する2点間の距離
+	if (yy1 < yy2) rad60 = acos((xx2 - xx1) / len) + 60.0 * M_PI / 180.0;
+	else rad60 = -acos((xx2 - xx1) / len) + 60.0 * M_PI / 180.0;
+	if (len < 0.03) return;
+
+	xx3 = xx1 + len * cos(rad60);
+	yy3 = yy1 + len * sin(rad60);
+
+
+	glVertex2d(x1, y1);
+	kochCurve(x1, y1, xx1, yy1);
+	glVertex2d(xx1, yy1);
+	kochCurve(xx1, yy1, xx3, yy3);
+	glVertex2d(xx3, yy3);
+	kochCurve(xx3, yy3, xx2, yy2);
+	glVertex2d(xx2, yy2);
+	kochCurve(xx2, yy2, x2, y2);
+	glVertex2d(x2, y2);
+}
+
+void display(void)
+{
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3d(1.0, 0.0, 1.0);
+	glBegin(GL_LINE_STRIP);
+
+	kochCurve(-0.5, -0.5, 0.5, -0.5);
+
+	glEnd();
+	glFlush();
+}
 
 int main(int argc, char *argv[])
 {
@@ -15,31 +56,4 @@ int main(int argc, char *argv[])
 	glutDisplayFunc(display);
 	glutMainLoop();
 	return 0;
-}
-
-void display(void)
-{
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3d(0.0, 1.0, 0.0);
-
-	glBegin(GL_LINES);
-	donut(100);
-	glEnd();
-
-	glFlush();
-}
-
-void donut(double delta)
-{
-	for (int i = 0; i < 360; i += 2)
-	{
-		// deltaの分だけ角度をずらす
-		double x1 = cos((i + delta)* M_PI / 180.0);
-		double y1 = sin((i + delta)* M_PI / 180.0);
-		glVertex2d(x1 * 0.5, y1 * 0.5);
-		double x2 = cos(i * M_PI / 180.0);
-		double y2 = sin(i * M_PI / 180.0);
-		glVertex2d(x2 * 1, y2 * 1);
-	}
 }
