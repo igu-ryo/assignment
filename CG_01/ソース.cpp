5dea08d74d79917cc16f81139200bbb067f4a95c
igu-ryo <49966687+igu-ryo@users.noreply.github.com>
2020-05-24 10:51:35 +0900

    課題1（初期状態）

--- CG_01/ソース.cpp
+++ CG_01/ソース.cpp
@@ -1,321 +1,75 @@
 #define FREEGLUT_STATIC
-#define _USE_MATH_DEFINES	// Visual Studio で 円周率として定数 M_PI を使用するため
 #include <cstdlib>
-#include <GL/glut.h>
+#include <cstdio>
 #include <cmath>
+#include <GL/glut.h>
 
-// 色の情報を格納する共用体
-union color {
-	struct { float r, g, b, a; };
-	float colors[4];
+using namespace std;
+
+// 3次元ベクトルを扱うためのクラス
+class Vector3d {
+public:
+	double x, y, z;
+	Vector3d() { x = y = z = 0; }
+	Vector3d(double _x, double _y, double _z) { x = _x; y = _y; z = _z; }
+	void set(double _x, double _y, double _z) { x = _x; y = _y; z = _z; }
+
+	// 長さを1に正規化する
+	void normalize() {
+		double len = length();
+		x /= len; y /= len; z /= len;
+	}
+
+	// 長さを返す
+	double length() { return sqrt(x * x + y * y + z * z); }
+
+	// s倍する
+	void scale(const double s) { x *= s; y *= s; z *= s; }
+
+	// 加算の定義
+	Vector3d operator+(Vector3d v) { return Vector3d(x + v.x, y + v.y, z + v.z); }
+
+	// 減算の定義
+	// ★加算の定義を参考に、減算のコードを記述してみる
+
+	// 内積の定義
+	double operator*(Vector3d v) { return x * v.x + y * v.y + z * v.z; }
+
+	// 外積の定義
+	Vector3d operator%(Vector3d v) { return Vector3d(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
+
+	// 代入演算の定義
+	Vector3d& operator=(const Vector3d& v) { x = v.x; y = v.y; z = v.z; return (*this); }
+
+	// 加算代入の定義
+	Vector3d& operator+=(const Vector3d& v) { x += v.x; y += v.y; z += v.z; return (*this); }
+
+	// 減算代入の定義
+	// ★加算代入の定義を参考に、減算代入のコードを記述してみる
+
+	// 値を出力する
+	void print() { printf("Vector3d(%f %f %f)\n", x, y, z); }
 };
 
-// 個々のティーポットの色や、傾き角度に関する情報を保持するための構造体
-struct TeapotData {
-	color ambient, diffuse, specular;
-	float shininess, angle;
-};
+// マイナスの符号の付いたベクトルを扱えるようにするための定義 例：b=(-a); のように記述できる
+Vector3d operator-(const Vector3d& v) { return(Vector3d(-v.x, -v.y, -v.z)); }
 
-// グローバル変数（プログラム中のどこからでもアクセスできる変数）には g_ を付けている
-const int g_NumTeapots = 8;
-TeapotData g_Teapots[g_NumTeapots];
+// ベクトルと実数の積を扱えるようにするための定義 例： c=5*a+2*b; c=b*3; のように記述できる
+Vector3d operator*(const double& k, const Vector3d& v) { return(Vector3d(k * v.x, k * v.y, k * v.z)); }
+Vector3d operator*(const Vector3d& v, const double& k) { return(Vector3d(v.x * k, v.y * k, v.z * k)); }
 
-// float型の値は、数字の後ろにfを付ける。末尾のゼロは省略できる
-float g_LightPosDegree;
-const float g_TeapotSize = 1.f;
-const float g_InnerRadius = 6.f;
-const float g_OuterRadius = 7.5f;
-const float g_HeightAmplitude = 0.8f;
-const float g_HeightOffset = 0.2f;
-float g_TeapotHeight[] = { -0.5f, 0.2f, -0.4f, 0.3f, -0.8f, 0.1f, -0.7f, 0.4f};
-const float g_TeapotHeightOffset = 1.f;
-const float g_DeltaTeapotHeight = 0.02f;
-const float g_LimitTeapotHeight = 0.8f;
-bool g_TeapotUpFlg[g_NumTeapots];
+// ベクトルを実数で割る操作を扱えるようにするための定義 例： c=a/2.3; のように記述できる
+Vector3d operator/(const Vector3d& v, const double& k) { return(Vector3d(v.x / k, v.y / k, v.z / k)); }
 
-const float g_EyeCenterX = 30.f;
-const float g_EyeCenterY = 9.f;
-const float g_EyeCenterZ = 30.f;
-const float g_EyeRadius = 16.f;
-float g_EyeX, g_EyeY, g_EyeZ, g_EyeDegree;
-
-const int g_AnimationIntervalMsec = 10;
-
-float g_RotationDegree = 0.f;
-const float g_DeltaRotationDegree = 3.f;
-
-int g_WindowWidth = 512;
-int g_WindowHeight = 512;
-
-// 円筒を描画…引数は円の半径、高さ、円の分割数
-// glutには円筒を描画するための関数が無いので、独自に準備
-void displayCylinder(float radius, float height, int nSlices) {
-	// 天頂面
-	const float deltaTheta = 2 * M_PI / (float)nSlices;
-
-	glNormal3f(0, 1, 0);
-	glBegin(GL_TRIANGLE_FAN);
-	glVertex3f(0, height, 0);
-	for (int i = 0; i <= nSlices; i++) {
-		const float theta = deltaTheta * i;
-		glVertex3f(radius * cosf(theta), height, radius * sinf(theta));
-	}
-	glEnd();
-
-	// 底面
-	glNormal3f(0, -1, 0);
-	glBegin(GL_TRIANGLE_FAN);
-	glVertex3f(0, 0, 0);
-	for (int i = 0; i <= nSlices; i++) {
-		const float theta = deltaTheta * i;
-		glVertex3f(radius * cosf(theta), 0, radius * sinf(theta));
-	}
-	glEnd();
-
-	// 側面
-	glBegin(GL_TRIANGLE_STRIP);
-	for (int i = 0; i <= nSlices; i++) {
-		const float theta = deltaTheta * i;
-		const float cosTheta = cosf(theta);
-		const float sinTheta = sinf(theta);
-		glNormal3f(cosTheta, 0, sinTheta);
-		glVertex3f(radius * cosTheta, height, radius * sinTheta);
-		glVertex3f(radius * cosTheta, 0, radius * sinTheta);
-	}
-	glEnd();
-}
-
-void display() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	// 透視投影変換の設定
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(30.0, g_WindowWidth / (double)g_WindowHeight, 1, 100.0);
-
-	// モデル座標の操作へモード切り替え
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0.0, g_EyeY, g_EyeZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-
-	float ambientColor[] = { 0.4f, 0.2f, 0.2f, 1.0f };
-	float diffuseColor[] = { 1.f, 0.7f, 0.4f, 1.0f };
-	float specularColor[] = { 0.2f, 0.4f, 0.3f, 1.0f };
-	float shininess = 20.f;
-
-	glMaterialfv(GL_FRONT, GL_AMBIENT, ambientColor);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuseColor);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, specularColor);
-	glMaterialfv(GL_FRONT, GL_SHININESS, &shininess);
-
-	// こまを傾けてさらに回転させる
-	glRotatef(g_RotationDegree, 0, 1, 0);
-	glRotatef(20, 0, 0, 1);
-
-	glRotatef(g_RotationDegree, 0, 1, 0); // 回転させている
-
-	// 屋根
-	glPushMatrix();
-	glTranslatef(0, g_HeightAmplitude + g_HeightOffset + 3.f, 0);
-	glRotatef(-90, 1, 0, 0);
-	glutSolidCone(g_OuterRadius, 2.f, 32, 32);
-	glPopMatrix();
-
-	// 中心の柱
-	glPushMatrix();
-	glTranslatef(0, -1.f, 0);
-	displayCylinder(0.5f, g_HeightAmplitude + g_HeightOffset + 6.5f, 32);
-	glPopMatrix();
-
-	// 土台(こま)
-	glPushMatrix();
-	glTranslatef(0, -1.f, 0);
-	glRotatef(90, 1, 0, 0);
-	glutSolidCone(g_OuterRadius, 5.f, 32, 32);
-	glPopMatrix();
-
-	// 屋根の上のティーポット
-	glPushMatrix();
-	glTranslatef(0, g_HeightAmplitude + g_HeightOffset + 5.5f, 0);
-	glutSolidTeapot(g_TeapotSize);
-	glPopMatrix();
-
-	const float deltaTheta = 360 / (float)g_NumTeapots;
-
-	// ティーポットと柱を1つずつ描画する
-	// ★下記のコードでは、常に同じ位置に描画されるので、全体が回転するように変更する
-	for (int i = 0; i < g_NumTeapots; i++) {
-		const float thetaDegree = deltaTheta * i; // ティーポットの位置を決めるための角度
-
-		const float thetaRad = thetaDegree * M_PI / 180.f;
-		const float xPos = g_InnerRadius * sinf(thetaRad);
-		const float zPos = g_InnerRadius * cosf(thetaRad);
-
-		// ティーポットの高さ方向の値
-		const float yPos = g_HeightOffset + g_TeapotHeightOffset + g_TeapotHeight[i]; // ★この値を少しずつ変化させることでティーポットが上下に移動する
-
-		// ティーポットの色の指定
-		glMaterialfv(GL_FRONT, GL_AMBIENT, g_Teapots[i].ambient.colors);
-		glMaterialfv(GL_FRONT, GL_DIFFUSE, g_Teapots[i].diffuse.colors);
-		glMaterialfv(GL_FRONT, GL_SPECULAR, g_Teapots[i].specular.colors);
-		glMaterialfv(GL_FRONT, GL_SHININESS, &g_Teapots[i].shininess);
-
-		// ティーポットの描画
-		glPushMatrix();
-		glTranslatef(xPos, yPos, zPos);
-		glRotatef(thetaDegree, 0, 1, 0);
-		glRotatef(g_Teapots[i].angle, 0, 0, 1);
-		glutSolidTeapot(1.2f * g_TeapotSize);
-		glPopMatrix();
-
-		// ティーポットを支える柱の色の指定 
-		glMaterialfv(GL_FRONT, GL_AMBIENT, ambientColor);
-		glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuseColor);
-		glMaterialfv(GL_FRONT, GL_SPECULAR, specularColor);
-		glMaterialfv(GL_FRONT, GL_SHININESS, &shininess);
-
-		// ティーポットを支える柱の描画
-		glPushMatrix();
-		glTranslatef(xPos, -1.f, zPos);
-		displayCylinder(0.3f, yPos + 1.f, 32);
-		glPopMatrix();
-	}
-
-	glutSwapBuffers();
-}
-
-float frand() { return rand() / (float)RAND_MAX; }
-
-// 初期設定を行う関数
-void init() {
-	glClearColor(1, 1, 1, 1);
-	glClearDepth(100.f);
-
-	float lightAmbientColor0[] = { 0.2f, 0.2f, 0.2f, 0.0f };
-	float lightDiffuseColor0[] = { 0.4f, 0.4f, 0.4f, 0.0f };
-	float lightSpecularColor0[] = { 0.8f, 0.8f, 0.8f, 0.0f };
-	float lightPosition0[] = { 5.0f, 5.0f * cos(g_LightPosDegree), 8.0f, 0.0f };
-
-	float lightAmbientColor1[] = { 0.2f, 0.2f, 0.2f, 0.0f };
-	float lightDiffuseColor1[] = { 0.4f, 0.4f, 0.4f, 0.0f };
-	float lightSpecularColor1[] = { 0.8f, 0.8f, 0.8f, 0.0f };
-	float lightPosition1[] = { -5.0f, 2.0f, 3.0f, 0.0f };
-
-	glEnable(GL_LIGHTING);
-
-	glEnable(GL_LIGHT0);
-	glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbientColor0);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuseColor0);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpecularColor0);
-	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition0);
-
-	glEnable(GL_LIGHT1);
-	glLightfv(GL_LIGHT1, GL_AMBIENT, lightAmbientColor1);
-	glLightfv(GL_LIGHT1, GL_DIFFUSE, lightDiffuseColor1);
-	glLightfv(GL_LIGHT1, GL_SPECULAR, lightSpecularColor1);
-	glLightfv(GL_LIGHT1, GL_POSITION, lightPosition1);
-
-	srand(0);
-
-	// 個々のティーポットの色を設定する処理 乱数で決めている
-	for (int i = 0; i < g_NumTeapots; i++) {
-		g_Teapots[i].ambient.r = 0.2f * frand();
-		g_Teapots[i].ambient.g = 0.2f * frand();
-		g_Teapots[i].ambient.b = 0.2f * frand();
-		g_Teapots[i].ambient.a = 1.f;
-
-		g_Teapots[i].diffuse.r = 0.2f * frand() + 0.8f;
-		g_Teapots[i].diffuse.g = 0.2f * frand() + 0.8f;
-		g_Teapots[i].diffuse.b = 0.2f * frand() + 0.8f;
-		g_Teapots[i].diffuse.a = 1.f;
-
-		g_Teapots[i].specular.r = 0.3f * frand() + 0.2f;
-		g_Teapots[i].specular.g = 0.3f * frand() + 0.2f;
-		g_Teapots[i].specular.b = 0.3f * frand() + 0.2f;
-		g_Teapots[i].specular.a = 1.f;
-
-		g_Teapots[i].shininess = 2.f + 30 * frand();
-
-		g_Teapots[i].angle = 15 * (2.f * frand() - 1.f);
-	}
-
-	glEnable(GL_DEPTH_TEST);
-}
-
-// 一定時間ごとに実行される関数
-void timer(int val) {
-	// 光源の位置を変更
-	g_LightPosDegree += 0.01f;
-	float lightPosition0[] = { 5.0f, 5.0f * cos(g_LightPosDegree), 8.0f, 0.0f };
-	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition0);
-
-	for (int i = 0; i < g_NumTeapots; i++)
-	{
-		// ティーポットの高さが上限と下限を超えたらフラグを変える
-		if (g_TeapotHeight[i] < -g_LimitTeapotHeight)
-		{
-			g_TeapotUpFlg[i] = true;
-		}
-		else if (g_TeapotHeight[i] > g_LimitTeapotHeight)
-		{
-			g_TeapotUpFlg[i] = false;
-		}
-
-		// ティーポットの高さを変更
-		if (g_TeapotUpFlg[i] == true)
-		{
-			g_TeapotHeight[i] += g_DeltaTeapotHeight;
-		}
-		else
-		{
-			g_TeapotHeight[i] -= g_DeltaTeapotHeight;
-		}
-	}
-
-
-	// 回転角度の更新
-	g_RotationDegree += g_DeltaRotationDegree;
-
-	const float rotationRad = 2.f * g_RotationDegree * M_PI / 180.f;
-
-	// ★ 下のコードでは視点が固定だけど
-	// ここで  g_EyeY と g_EyeZ の値を変えることで視点位置を変化させることができる
-	g_EyeDegree += 0.01f;
-	g_EyeX = g_EyeCenterX + g_EyeRadius * cos(g_EyeDegree);
-	g_EyeY = g_EyeCenterY + g_EyeRadius * cos(g_EyeDegree);
-	g_EyeZ = g_EyeCenterZ + g_EyeRadius * cos(g_EyeDegree);
-
-	glutPostRedisplay();
-
-	glutTimerFunc(g_AnimationIntervalMsec, timer, val);
-}
-
-// ウィンドウサイズが変更されたときの処理
-void reshape(int w, int h) {
-	if (h < 1) return;
-
-	// ビューポートをウィンドウサイズに変更
-	glViewport(0, 0, w, h);
-	g_WindowWidth = w;
-	g_WindowHeight = h;
-}
 
 int main(int argc, char** argv) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(g_WindowWidth, g_WindowHeight);
+	// ベクトルの加算の例
+	Vector3d a(1, 1, 1);
+	Vector3d b(3, 2, -1);
+	Vector3d c = a + b;
+	c.print();
 
-	// ウィンドウタイトルに表示する文字列を指定する場合
-	glutCreateWindow("Teapot Merry-Go-Round");
-
-	glutDisplayFunc(display);
-	glutReshapeFunc(reshape); // ウィンドウサイズが変更されたときに実行される関数を指定
-	glutTimerFunc(g_AnimationIntervalMsec, timer, 0);
-
-	// 初期設定を行う
-	init();
-
-	glutMainLoop();
-
+	// Visual Studio でコンソールがすぐに閉じないようにするためには次のコメントアウトをはずす
+	//system("pause"); 
 	return 0;
 }
